bc881bacc66fdf1f89f7032df63e6d8552991d03
Taybin Rutkin <trutkin@utest.com>
2013-06-01 22:03:53 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Lexthink
+
+** TODO: Add description **
---
+++ mix.exs
@@ -0,0 +1,20 @@
+defmodule Lexthink.Mixfile do
+  use Mix.Project
+
+  def project do
+    [ app: :lexthink,
+      version: "0.0.1",
+      deps: deps ]
+  end
+
+  # Configuration for the OTP application
+  def application do
+    [mod: { Lexthink, [] }]
+  end
+
+  # Returns the list of dependencies in the format:
+  # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
+  defp deps do
+    []
+  end
+end
