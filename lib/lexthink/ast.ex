b0a4022bede7a82ebc2e2c36a3f5c2347dfc3d5f
Taybin Rutkin <trutkin@utest.com>
2013-06-25 23:07:09 -0400

    more work on AST items

--- lib/lexthink/ast.ex
+++ lib/lexthink/ast.ex
@@ -1,79 +1,85 @@
 defmodule Lexthink.AST do
-
-  @type build_result :: :term | {:error, binary}
-
-  @spec db_create(binary) :: build_result
+  @spec db_create(binary) :: :term
   def db_create(name), do: :term.new(type: :'DB_CREATE', args: expr(name))
 
-  @spec db_drop(binary) :: build_result
+  @spec db_drop(binary) :: :term
   def db_drop(name), do: :term.new(type: :'DB_DROP', args: expr(name))
 
-  @spec db_list() :: build_result
+  @spec db_list() :: :term
   def db_list(), do: :term.new(type: :'DB_LIST')
 
-  #@spec table_create(binary, [lethink:table_options]) :: build_result
-  def table_create(name, options) do
+  @spec table_create(binary) :: :term
+  def table_create(name) when is_binary(name) do
+    table_create(name, [])
+  end
+
+  @spec table_create(binary, Keyword) :: :term
+  def table_create(name, options) when is_binary(name) do
     optargs = lc opt inlist options, do: table_option_term(opt)
     :term.new(type: :'TABLE_CREATE', args: expr(name), optargs: optargs)
   end
 
-  #@spec table_create(:term, binary, [lethink:table_options]) :: build_result
+  @spec table_create(:term, binary) :: :term
+  def table_create(db, name) when is_binary(name) do
+    table_create(db, name, [])
+  end
+
+  @spec table_create(:term, binary, Keyword) :: :term
   def table_create(:term[type: :'DB'] = db, name, options) do
     optargs = lc opt inlist options, do: table_option_term(opt)
     :term.new(type: :'TABLE_CREATE', args: [db, expr(name)], optargs: optargs)
   end
 
-  #@private
-  #-spec table_option_term(lethink:table_options()) -> #term_assocpair{}.
-  defp table_option_term({atom, value}) when is_atom(atom) do
-      term_assocpair(atom_to_binary(atom, :utf8), value)
-  end
-
-  @spec table_drop(binary) :: build_result
+  @spec table_drop(binary) :: :term
   def table_drop(name) do
     :term.new(type: :'TABLE_DROP', args: expr(name))
   end
 
-  @spec table_drop(:term, binary) :: build_result
+  @spec table_drop(:term, binary) :: :term
   def table_drop(:term[ type: :'DB' ] = db, name) do
     :term.new(type: :'TABLE_DROP', args: [db, expr(name)])
   end
 
-  @spec table_list() :: build_result
+  @spec table_list() :: :term
   def table_list(), do: :term.new(type: :'TABLE_LIST')
 
-  @spec table_list(:term) :: build_result
-  def table_list(:term[type: :'DB'] = db), do: :term.new(type: :'TABLE_LIST', args: db)
+  @spec table_list(:term) :: :term
+  def table_list(:term[type: :'DB'] = db) do
+    :term.new(type: :'TABLE_LIST', args: db)
+  end
 
   #%% @doc Specify a DB.  Must be first operation in query list
   #%% Optional if a default database has been specified via
   #%% @see lethink:use/2
-  @spec db(binary) :: build_result
+  @spec db(binary) :: :term
   def db(name), do: :term.new(type: :'DB', args: expr(name))
 
-  @spec table(binary) :: build_result
-  def table(name), do: table([], name, :false)
+  @spec table(binary) :: :term
+  def table(name), do: table([], name, [])
 
-  @spec table(binary, [] | :term) :: build_result
-  def table(name, :term[type: :'DB'] = db), do: table(name, :false, db)
+  @spec table(:term | binary, binary | Keyword) :: :term
+  def table(:term[type: :'DB'] = db, name), do: table(db, name, [])
+  def table(name, options) when is_binary(name) do
+    table([], name, options)
+  end
 
-  @spec table(binary, boolean, [] | :term) :: build_result
-  def table(name, use_outdated, []) when is_binary(name) do
-    optargs = [term_assocpair("use_outdated", use_outdated)]
+  @spec table(:term | [], binary, Keyword) :: :term
+  def table([], name, options) do
+    optargs = lc opt inlist options, do: table_option_term(opt)
     :term.new(type: :'TABLE', args: expr(name), optargs: optargs)
   end
-  def table(name, use_outdated, :term[type: :'DB'] = db) when is_binary(name) do
-    optargs = [term_assocpair("use_outdated", use_outdated)]
+  def table(db, name, options) do
+    optargs = lc opt inlist options, do: table_option_term(opt)
     :term.new(type: :'TABLE', args: [db, expr(name)], optargs: optargs)
   end
 
-  #@spec insert([:document], :term) :: build_result
+  @spec insert([:document], :term) :: :term
   def insert(:term[type: :'TABLE'] = table, data) do
       :io.fwrite("~p~n", [data])
       :term.new(type: :'INSERT', args: [table] ++ lc d inlist data, do: expr(d))
   end
 
-  #@spec insert([#document{}], [lethink:insert_options()], #term{}) -> build_result().
+  #@spec insert([#document{}], [lethink:insert_options()], #term{}) -> :term().
   def insert(:term[type: :'TABLE'] = table, data, options) do
     args = [table] ++ lc d inlist data, do: expr(d)
     optargs = lc opt inlist options, do: insert_option_term(opt)
@@ -86,92 +92,92 @@ defmodule Lexthink.AST do
     term_assocpair(atom_to_binary(:upsert, :utf8), value)
   end
 
-  @spec get(binary | number, :term) :: build_result
+  @spec get(binary | number, :term) :: :term
   def get(:term[type: :'TABLE'] = table, key) when is_binary(key) or is_number(key) do
     :term.new(type: :'GET', args: [table | expr(key)])
   end
 
-  @spec update(:term, HashDict | fun) :: build_result
+  @spec update(:term, HashDict | fun) :: :term
   def update(selection, data) do
     :term.new(type: :'UPDATE', args: [selection | func_wrap(data)])
   end
 
-  @spec row() :: build_result
+  @spec row() :: :term
   def row(), do: :term.new(type: :'IMPLICIT_VAR')
 
-  @spec getattr(binary, :term) :: build_result
+  @spec getattr(binary, :term) :: :term
   def getattr(term, attr) do
     term.new(type: :'GETATTR', args: [term | expr(attr)])
   end
 
   #%% Math and Logic Operations
 
-  @spec add(:term, number | binary) :: build_result
+  @spec add(:term, number | binary) :: :term
   def add(term, value) do
     :term.new(type: :'ADD', args: [term | expr(value)])
   end
 
-  @spec sub(:term, number) :: build_result
+  @spec sub(:term, number) :: :term
   def sub(term, value) do
     :term.new(type: :'SUB', args: [term | expr(value)])
   end
 
-  @spec mul(:term, number) :: build_result
+  @spec mul(:term, number) :: :term
   def mul(term, value) do
     :term.new(type: :'MUL', args: [term | expr(value)])
   end
 
-  @spec div(:term, number) :: build_result
+  @spec div(:term, number) :: :term
   def div(term, value) do
     :term.new(type: :'DIV', args: [term | expr(value)])
   end
 
-  @spec mod(:term, number) :: build_result
+  @spec mod(:term, number) :: :term
   def mod(term, value) do
     :term.new(type: :'MOD', args: [term | expr(value)])
   end
 
-  @spec and_(:term, number) :: build_result
+  @spec and_(:term, number) :: :term
   def and_(term, value) do
     :term.new(type: :'AND', args: [term | expr(value)])
   end
 
-  @spec or_(:term, number) :: build_result
+  @spec or_(:term, number) :: :term
   def or_(term, value) do
     :term.new(type: :'OR', args: [term | expr(value)])
   end
 
-  @spec eq(:term, number) :: build_result
+  @spec eq(:term, number) :: :term
   def eq(term, value) do
     :term.new(type: :'EQ', args: [term | expr(value)])
   end
 
-  @spec ne(:term, number) :: build_result
+  @spec ne(:term, number) :: :term
   def ne(term, value) do
     :term.new(type: :'NE', args: [term | expr(value)])
   end
 
-  @spec gt(:term, number) :: build_result
+  @spec gt(:term, number) :: :term
   def gt(term, value) do
     :term.new(type: :'GT', args: [term | expr(value)])
   end
 
-  @spec ge(:term, number) :: build_result
+  @spec ge(:term, number) :: :term
   def ge(term, value) do
     :term.new(type: :'GE', args: [term | expr(value)])
   end
 
-  @spec lt(:term, number) :: build_result
+  @spec lt(:term, number) :: :term
   def lt(term, value) do
     :term.new(type: :'LT', args: [term | expr(value)])
   end
 
-  @spec le(:term, number) :: build_result
+  @spec le(:term, number) :: :term
   def le(term, value) do
     :term.new(type: :'LE', args: [term | expr(value)])
   end
 
-  @spec not_(:term) :: build_result
+  @spec not_(:term) :: :term
   def not_(term) do
     :term.new(type: :'NOT', args: [term])
   end
@@ -248,4 +254,9 @@ defmodule Lexthink.AST do
   end
   defp ivar_scan(_), do: :false
 
+  @private
+  @spec table_option_term({atom, any}) :: :term_assocpair
+  defp table_option_term({atom, value}) when is_atom(atom) do
+      term_assocpair(atom_to_binary(atom, :utf8), value)
+  end
 end
